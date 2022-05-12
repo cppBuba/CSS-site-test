adaa095a7442a0988eac3284884205825281de9d
Buba <jakub.bednarczyk@hotmail.com>
2022-05-12 22:30:02 +0200

    utf i pl

--- scripts/clock.js
+++ scripts/clock.js
@@ -11,13 +11,13 @@ var timeleft = countDownDate - now;
 var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
 var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-    
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);    
+
 // Result is output to the specific element
 document.getElementById("days").innerHTML = days + "d "
 document.getElementById("hours").innerHTML = hours + "h " 
 document.getElementById("mins").innerHTML = minutes + "m " 
-document.getElementById("secs").innerHTML = seconds + "s " 
+document.getElementById("secs").innerHTML = seconds + "s "
     
 // Display the message when countdown is over
 if (timeleft < 0) {
